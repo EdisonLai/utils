2b4365d1fbc3b718aef5f848f2596108b3f89830
laixuefeng <laixuefeng@didiglobal.com>
2021-12-01 15:08:55 +0800

    mysql connect function

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/EdisonLai/utils
+
+go 1.15
+
+require github.com/jinzhu/gorm v1.9.16
---
+++ mysql/mysql.go
@@ -0,0 +1,20 @@
+package mysql
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var db *gorm.DB
+
+func InitMySQLConnection(addr string) (err error) {
+	db, err = gorm.Open("mysql", addr)
+	if err != nil {
+		err = fmt.Errorf("%v: %s", err, "Building mysql connection failed!")
+		return err
+	}
+	db.DB().SetMaxIdleConns(1)
+	db.DB().SetMaxOpenConns(50)
+	return nil
+}
